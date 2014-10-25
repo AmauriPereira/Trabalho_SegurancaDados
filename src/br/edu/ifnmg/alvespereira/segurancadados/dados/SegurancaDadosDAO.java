e5b1884ba8e480917880fafc8c683ea7c82560a4
Amauri <Amauri@Amauri-PC>
2014-10-25 21:56:38 -0200

    crição da classe BancoDadosDAO

---
+++ src/br/edu/ifnmg/alvespereira/segurancadados/dados/SegurancaDadosDAO.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.alvespereira.segurancadados.dados;
+
+import br.edu.ifnmg.alvespereira.segurancadados.entidades.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @author Amauri
+ */
+public class SegurancaDadosDAO {
+
+    private static final String SQL_INSERT = "INSERT INTO USUARIO( SENHA,NOME )VALUES (?,?)";
+
+    public void criaUsuario(Usuario usuario) throws SQLException {
+        Connection conexao = null;
+        PreparedStatement comando = null;
+
+        try {
+            //recupera  a conexão com o banco de dados
+            conexao = BancoDadosUtil.getConnection();
+            
+            //Comando para enserie dados no banco de dados
+            comando = conexao.prepareStatement(SQL_INSERT);
+            
+            //atribui os parametros
+            comando.setString(2, usuario.getNome());
+            comando.setString(3, usuario.getEmail());
+            comando.setString(4, usuario.getSenha());
+            
+            //Executa o comando 
+            comando.execute();
+            
+            //Persiste o comando no banco de dados 
+            conexao.commit();
+        } catch (Exception e) {
+            /*Caso aconteça alguma exeção é feito um rollback para o banco de
+             dados retornar ao seu estado anterior. */
+            if (conexao != null) {
+                conexao.rollback();
+            }
+            //Lança a exceção do tipo sem verificação 
+            throw new RuntimeException(e);
+        } finally {
+            //Todo objeto que referencie o banco de dados deve ser fechado 
+            if (comando != null && !comando.isClosed()) {
+                comando.close();
+            }
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        }
+    }
+
+}
