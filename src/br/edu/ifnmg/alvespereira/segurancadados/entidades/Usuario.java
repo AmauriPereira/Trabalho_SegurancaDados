0f746724e0c62ddc3ac1595f00ef66d5aed5f493
Amauri <Amauri@Amauri-PC>
2014-10-25 21:14:27 -0200

    criação da classe usuario

---
+++ src/br/edu/ifnmg/alvespereira/segurancadados/entidades/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.alvespereira.segurancadados.entidades;
+
+/**
+ *
+ * @author Amauri
+ */
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String tipo;
+    private Departamento departamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+}
