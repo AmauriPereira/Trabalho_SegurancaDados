b7b12781bd8760790a8bb5b844f6eff455842a07
Amauri <Amauri@Amauri-PC>
2014-11-21 19:23:55 -0200

    criação da entidade Projeto

---
+++ src/br/edu/ifnmg/alvespereira/segurancadados/entidades/Projeto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.alvespereira.segurancadados.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Amauri
+ */
+public class Projeto {
+
+    private String nome;
+    private String descricao;
+    private Date dataInicio;
+    private Date dataTermino;
+    private Usuario gerente;
+    private Departamento departamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(Date dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    public Usuario getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(Usuario gerente) {
+        this.gerente = gerente;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+}
