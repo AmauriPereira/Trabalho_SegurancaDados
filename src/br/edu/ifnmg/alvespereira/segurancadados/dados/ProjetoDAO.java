8bc53a720bce5fb7a76864d06a16c76b46de855f
Amauri <Amauri@Amauri-PC>
2014-11-22 14:31:19 -0200

    alteração classe  ProjetoDAO - metodo de cbProjetos

--- src/br/edu/ifnmg/alvespereira/segurancadados/dados/ProjetoDAO.java
+++ src/br/edu/ifnmg/alvespereira/segurancadados/dados/ProjetoDAO.java
@@ -4,14 +4,17 @@ import br.edu.ifnmg.alvespereira.segurancadados.entidades.Projeto;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProjetoDAO {
 
     private static final String SQL_INSERT_PROJETO
             = "INSERT INTO PROJETO(NOME, DESCRICAO, DATA_INCIO, DATA_TERMINO, COD_DEPARTAMENTO) "
             + "VALUES (?,?,?,?,?)";
-
+    
+    private static final String SQL_SELECT_TODOS_PROJETOS = "SELECT NOME FROM PROJETO";
     public void criarProjeto(Projeto projeto) throws SQLException {
         Connection conexao = null;
         PreparedStatement comando = null;
@@ -44,5 +47,43 @@ public class ProjetoDAO {
                 conexao.close();
             }
         }
+    }
+
+    //SELECIONA TODOS OS DEPARTAMENTO, E ARMAZENA EM UMA LISTA
+    public ArrayList<String> cbProjetos() throws SQLException {
+        ArrayList<String> Projeto = new ArrayList<>();
+        Connection conexao = null;
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+
+        try {
+
+            conexao = BancoDadosUtil.getConnection();
+            comando = conexao.prepareStatement(SQL_SELECT_TODOS_PROJETOS);
+
+            resultado = comando.executeQuery();
+            Projeto.removeAll(Projeto);
+
+            while (resultado.next()) {
+                Projeto.add(resultado.getString("NOME"));
+            }
+
+            conexao.commit();
+
+        } catch (Exception e) {
+            if (conexao != null) {
+                conexao.rollback();
+            }
+            throw new RuntimeException(e);
+
+        } finally {
+            if (comando != null && !comando.isClosed()) {
+                comando.close();
+            }
+            if (conexao != null && !conexao.isClosed()) {
+                conexao.close();
+            }
+        }
+        return Projeto;
     }
 }
