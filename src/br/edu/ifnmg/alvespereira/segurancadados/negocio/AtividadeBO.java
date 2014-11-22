c95fdcd3d751aaaccf838c841c320670acc01338
Amauri <Amauri@Amauri-PC>
2014-11-22 14:33:26 -0200

    alteração classe  ProjetoDAO -  alteração

--- src/br/edu/ifnmg/alvespereira/segurancadados/negocio/AtividadeBO.java
+++ src/br/edu/ifnmg/alvespereira/segurancadados/negocio/AtividadeBO.java
@@ -1,5 +1,16 @@
 package br.edu.ifnmg.alvespereira.segurancadados.negocio;
 
+import br.edu.ifnmg.alvespereira.segurancadados.dados.AtividadeDAO;
+import br.edu.ifnmg.alvespereira.segurancadados.entidades.Atividade;
+import java.sql.SQLException;
+
 public class AtividadeBO {
+    
+     public void criarAtividade(Atividade atividade) throws SQLException {
+        
+        AtividadeDAO atividadeDAO = new AtividadeDAO();
+        atividadeDAO.criarAtividade(atividade);
+
+    }
 
 }
