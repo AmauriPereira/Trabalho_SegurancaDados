957dac312fbe1d1af36e618b769a228379979d44
Amauri <Amauri@Amauri-PC>
2014-11-24 20:58:10 -0200

    atualização calsse TelaPrincipal

--- src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/TelaPrincipal.java
+++ src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/TelaPrincipal.java
@@ -105,12 +105,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
-        mnuExcluir = new javax.swing.JMenu();
-        itmMnu_Ex_Departamento = new javax.swing.JMenuItem();
-        itmMnu_Ex_Usuarios = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem7 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
         mnuOpcoes = new javax.swing.JMenu();
         itmMnuSair = new javax.swing.JMenuItem();
         jMenuItem9 = new javax.swing.JMenuItem();
@@ -274,7 +268,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         jMenuBar1.add(mnuCadastros);
 
         mnuAtualizar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/tous-refresh-reload-onglets-icone-7131-32.png"))); // NOI18N
-        mnuAtualizar.setText("    Atualizar     ");
+        mnuAtualizar.setText("Gestão de  Cadastros");
 
         itmMnu_Departamento.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, java.awt.event.InputEvent.ALT_MASK));
         itmMnu_Departamento.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/0258.png"))); // NOI18N
@@ -312,46 +306,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         mnuAtualizar.add(jMenuItem5);
 
         jMenuBar1.add(mnuAtualizar);
-
-        mnuExcluir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/gtk-ltr-undelete-icone-3875-32.png"))); // NOI18N
-        mnuExcluir.setText("    Excluir    ");
-
-        itmMnu_Ex_Departamento.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
-        itmMnu_Ex_Departamento.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/35.png"))); // NOI18N
-        itmMnu_Ex_Departamento.setText("     Departamento");
-        itmMnu_Ex_Departamento.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                itmMnu_Ex_DepartamentoActionPerformed(evt);
-            }
-        });
-        mnuExcluir.add(itmMnu_Ex_Departamento);
-
-        itmMnu_Ex_Usuarios.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F2, 0));
-        itmMnu_Ex_Usuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/bill-homme-personne-utilisateur-icone-6596-32.png"))); // NOI18N
-        itmMnu_Ex_Usuarios.setText("     Gerente");
-        itmMnu_Ex_Usuarios.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                itmMnu_Ex_UsuariosActionPerformed(evt);
-            }
-        });
-        mnuExcluir.add(itmMnu_Ex_Usuarios);
-
-        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F3, 0));
-        jMenuItem6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/garcon-utilisateur-icone-7572-32.png"))); // NOI18N
-        jMenuItem6.setText("     Encarregado");
-        mnuExcluir.add(jMenuItem6);
-
-        jMenuItem7.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, 0));
-        jMenuItem7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/liste-texte-vue-icone-4177-32.png"))); // NOI18N
-        jMenuItem7.setText("     Projeto");
-        mnuExcluir.add(jMenuItem7);
-
-        jMenuItem8.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, 0));
-        jMenuItem8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/cabinet-dossiers-icone-8056-32.png"))); // NOI18N
-        jMenuItem8.setText("     Atividade");
-        mnuExcluir.add(jMenuItem8);
-
-        jMenuBar1.add(mnuExcluir);
 
         mnuOpcoes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/engrenages-package-systeme-roues-icone-8982-32.png"))); // NOI18N
         mnuOpcoes.setText("      Opções    ");
@@ -395,10 +349,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void itmMnu_Ex_DepartamentoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmMnu_Ex_DepartamentoActionPerformed
-
-    }//GEN-LAST:event_itmMnu_Ex_DepartamentoActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         //metodo que seta a hora e data atual no sistema(Na barra de status)
@@ -518,10 +468,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_itmMnuSairActionPerformed
 
-    private void itmMnu_Ex_UsuariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmMnu_Ex_UsuariosActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_itmMnu_Ex_UsuariosActionPerformed
-
     private void itmMnu_DepartamentoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmMnu_DepartamentoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_itmMnu_DepartamentoActionPerformed
@@ -639,16 +585,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem itmMnuProjeto;
     private javax.swing.JMenuItem itmMnuSair;
     private javax.swing.JMenuItem itmMnu_Departamento;
-    private javax.swing.JMenuItem itmMnu_Ex_Departamento;
-    private javax.swing.JMenuItem itmMnu_Ex_Usuarios;
     private javax.swing.JMenuItem itmMnu_Usuarios;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblData;
@@ -658,7 +599,6 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel lblUsuarioLogado;
     private javax.swing.JMenu mnuAtualizar;
     private javax.swing.JMenu mnuCadastros;
-    private javax.swing.JMenu mnuExcluir;
     private javax.swing.JMenu mnuOpcoes;
     private javax.swing.JLabel txtDepartamento;
     private javax.swing.JLabel txtFuncao;
