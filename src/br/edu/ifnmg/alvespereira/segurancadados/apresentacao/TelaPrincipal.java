95812efb693a3920522d6ad9294d7110fb922f40
Amauri <Amauri@Amauri-PC>
2014-11-26 09:52:47 -0200

    modificações na tela  tela principal

--- src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/TelaPrincipal.java
+++ src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/TelaPrincipal.java
@@ -98,10 +98,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
         itmMnuAtividade = new javax.swing.JMenuItem();
         mnuAtualizar = new javax.swing.JMenu();
         itmMnu_Departamento = new javax.swing.JMenuItem();
-        itmMnu_Usuarios = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
+        itmMnu_Gerente = new javax.swing.JMenuItem();
+        itmMnu_Encarregado = new javax.swing.JMenuItem();
         MenuProjeto = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
+        itmMnu_Atividade = new javax.swing.JMenuItem();
         mnuOpcoes = new javax.swing.JMenu();
         jMenuItem9 = new javax.swing.JMenuItem();
         itmMnuSair = new javax.swing.JMenuItem();
@@ -277,25 +277,20 @@ public class TelaPrincipal extends javax.swing.JFrame {
         });
         mnuAtualizar.add(itmMnu_Departamento);
 
-        itmMnu_Usuarios.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F2, java.awt.event.InputEvent.ALT_MASK));
-        itmMnu_Usuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/bill-homme-personne-utilisateur-icone-6596-32.png"))); // NOI18N
-        itmMnu_Usuarios.setText("     Gerente   ");
-        itmMnu_Usuarios.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                itmMnu_UsuariosActionPerformed(evt);
-            }
-        });
-        mnuAtualizar.add(itmMnu_Usuarios);
+        itmMnu_Gerente.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F2, java.awt.event.InputEvent.ALT_MASK));
+        itmMnu_Gerente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/bill-homme-personne-utilisateur-icone-6596-32.png"))); // NOI18N
+        itmMnu_Gerente.setText("     Gerente   ");
+        mnuAtualizar.add(itmMnu_Gerente);
 
-        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F3, java.awt.event.InputEvent.ALT_MASK));
-        jMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/garcon-utilisateur-icone-7572-32.png"))); // NOI18N
-        jMenuItem3.setText("     Encarregado");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+        itmMnu_Encarregado.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F3, java.awt.event.InputEvent.ALT_MASK));
+        itmMnu_Encarregado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/garcon-utilisateur-icone-7572-32.png"))); // NOI18N
+        itmMnu_Encarregado.setText("     Encarregado");
+        itmMnu_Encarregado.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
+                itmMnu_EncarregadoActionPerformed(evt);
             }
         });
-        mnuAtualizar.add(jMenuItem3);
+        mnuAtualizar.add(itmMnu_Encarregado);
 
         MenuProjeto.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
         MenuProjeto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/liste-texte-vue-icone-4177-32.png"))); // NOI18N
@@ -307,10 +302,15 @@ public class TelaPrincipal extends javax.swing.JFrame {
         });
         mnuAtualizar.add(MenuProjeto);
 
-        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, java.awt.event.InputEvent.ALT_MASK));
-        jMenuItem5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/cabinet-dossiers-icone-8056-32.png"))); // NOI18N
-        jMenuItem5.setText("     Atividade");
-        mnuAtualizar.add(jMenuItem5);
+        itmMnu_Atividade.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, java.awt.event.InputEvent.ALT_MASK));
+        itmMnu_Atividade.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/alvespereira/segurancadados/icones/cabinet-dossiers-icone-8056-32.png"))); // NOI18N
+        itmMnu_Atividade.setText("     Atividade");
+        itmMnu_Atividade.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itmMnu_AtividadeActionPerformed(evt);
+            }
+        });
+        mnuAtualizar.add(itmMnu_Atividade);
 
         jMenuBar1.add(mnuAtualizar);
 
@@ -480,9 +480,9 @@ public class TelaPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_itmMnu_DepartamentoActionPerformed
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+    private void itmMnu_EncarregadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmMnu_EncarregadoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
+    }//GEN-LAST:event_itmMnu_EncarregadoActionPerformed
 
     private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
         // TODO add your handling code here:
@@ -593,6 +593,15 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     }//GEN-LAST:event_MenuProjetoActionPerformed
 
+    private void itmMnu_AtividadeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itmMnu_AtividadeActionPerformed
+        GerenciarAtvidadeFom gerenciarAtvidadeFom = null;
+        gerenciarAtvidadeFom = new GerenciarAtvidadeFom(usuarioLogado);
+        gerenciarAtvidadeFom.setVisible(true);
+        centralizaForm(gerenciarAtvidadeFom);
+        JDP1.add(gerenciarAtvidadeFom);
+
+    }//GEN-LAST:event_itmMnu_AtividadeActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Data;
     private javax.swing.JLabel Hora;
@@ -604,11 +613,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem itmMnuGerente;
     private javax.swing.JMenuItem itmMnuProjeto;
     private javax.swing.JMenuItem itmMnuSair;
+    private javax.swing.JMenuItem itmMnu_Atividade;
     private javax.swing.JMenuItem itmMnu_Departamento;
-    private javax.swing.JMenuItem itmMnu_Usuarios;
+    private javax.swing.JMenuItem itmMnu_Encarregado;
+    private javax.swing.JMenuItem itmMnu_Gerente;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblData;
