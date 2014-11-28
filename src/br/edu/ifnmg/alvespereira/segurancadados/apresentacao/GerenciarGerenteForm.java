4b859ab14c0ff96be16e32d9286e32c2c756a7d0
Amauri <Amauri@192.168.230.151>
2014-11-28 09:23:21 -0200

    Alteração GerenciarGerente

--- src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/GerenciarGerenteForm.java
+++ src/br/edu/ifnmg/alvespereira/segurancadados/apresentacao/GerenciarGerenteForm.java
@@ -123,10 +123,7 @@ public class GerenciarGerenteForm extends javax.swing.JInternalFrame {
 
         tbResultadoBusca.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
